5ffe415623c9669564daf728f24b6ba08198097e
Pedro Pontes García <pp457@cornell.edu>
2025-09-08 16:11:10 -0400

    Add per-function variable mapping tool

---
+++ property-analysis/src/main.rs
@@ -0,0 +1,56 @@
+use bril_rs::*;
+use clap::Parser;
+use std::{collections::{HashMap, HashSet}, fs::File, io::BufReader};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(value_hint = clap::ValueHint::FilePath, required=true)]
+    filename: Option<std::path::PathBuf>,
+}
+
+fn build_var_set(v: Program) -> HashMap<String, HashSet<String>> {
+    let mut var_map = HashMap::new();
+    for f in v.functions {
+        let mut var_set = HashSet::new();
+        for c in f.instrs {
+            match c {
+                Code::Instruction(Instruction::Constant { dest, .. })
+                | Code::Instruction(Instruction::Value { dest, .. }) => {
+                    var_set.insert(format!("{dest}"));
+
+                }
+                _ => {}
+            }
+        }
+        var_map.insert(f.name, var_set);
+    }
+    return var_map;
+}
+
+// usage: target/debug/rust_stuff test.json
+fn main() {
+    let args = Args::parse();
+
+    let filename = args
+        .filename
+        .expect("bad file! this shouldn't normally happen");
+
+    let file = match File::open(filename.as_path()) {
+        Err(why) => {
+            eprintln!("couldn't open file: {}", why);
+            std::process::exit(1)
+        }
+        Ok(file) => file,
+    };
+
+    let reader = BufReader::new(file);
+
+    let v: bril_rs::Program = match serde_json::from_reader(reader) {
+        Err(why) => panic!("{}", why),
+        Ok(v) => v,
+    };
+
+    let set = build_var_set(v);
+    print!("{:?}", set);
+}
