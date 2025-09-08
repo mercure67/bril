bb404ca895f9aee14dd39d7121c3ac376c40b51d
mercure67 <57854733+mercure67@users.noreply.github.com>
2025-09-08 11:39:42 -0400

    feat: block former

--- rust_stuff/src/main.rs
+++ rust_stuff/src/main.rs
@@ -1,12 +1,142 @@
 use bril_rs::*;
 use clap::{Parser, Subcommand};
-use std::{fs::File, io::BufReader};
+use std::collections::HashSet;
+use std::{fs::File, io::BufReader, rc::Rc};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(value_hint = clap::ValueHint::FilePath, required=true)]
     filename: Option<std::path::PathBuf>,
+}
+
+fn get_used_labels<'a>(v: &'a bril_rs::Program) -> HashSet<String> {
+    // return the list of used labels in the program
+    // currently pretty simple, doesn't do analysis of conditions / dead code / etc.
+    // also doesn't handle imports
+    //
+
+    // TODO: do labels need to be unique?
+    let mut res = HashSet::<String>::new();
+
+    for f in v.functions.iter() {
+        for i in f.instrs.iter() {
+            //TODO: the ergonomics of this part of the library are poor
+            if let Code::Instruction(inner) = i {
+                if let Instruction::Effect {
+                    args: _,
+                    funcs,
+                    labels,
+                    op,
+                    pos: _,
+                } = inner
+                {
+                    match op {
+                        EffectOps::Jump => {
+                            for x in labels.iter() {
+                                res.insert(x.clone());
+                            }
+                        }
+                        EffectOps::Branch => {
+                            for x in labels.iter() {
+                                res.insert(x.clone());
+                            }
+                        }
+                        EffectOps::Return => (),
+                        EffectOps::Call => {
+                            for x in funcs.iter() {
+                                res.insert(x.clone());
+                            }
+                        }
+                        _ => (),
+                    }
+                }
+                if let Instruction::Value {
+                    args: _,
+                    dest: _,
+                    funcs,
+                    labels: _,
+                    op,
+                    pos: _,
+                    op_type: _,
+                } = inner
+                {
+                    match op {
+                        ValueOps::Call => {
+                            for x in funcs.iter() {
+                                res.insert(x.clone());
+                            }
+                        }
+                        _ => (),
+                    }
+                }
+            }
+        }
+    }
+    res
+}
+
+type Block<'a> = (&'a String, Vec<&'a Code>);
+
+fn form_basic_blocks<'a>(v: &'a bril_rs::Program, used_labels: &HashSet<String>) -> Vec<Block<'a>> {
+    let mut res = Vec::<Block>::new();
+    for f in v.functions.iter() {
+        let mut new_block = Vec::<&'a Code>::new();
+        for instr in f.instrs.iter() {
+            match instr {
+                Code::Instruction(i) => {
+                    new_block.push(instr);
+                    match i {
+                        Instruction::Value {
+                            args: _,
+                            dest: _,
+                            funcs: _,
+                            labels: _,
+                            op,
+                            pos: _,
+                            op_type: _,
+                        } => {
+                            if let ValueOps::Call = op {
+                                res.push((&f.name, new_block));
+                                new_block = Vec::<&'a Code>::new();
+                            }
+                        }
+                        Instruction::Effect {
+                            args: _,
+                            funcs: _,
+                            labels: _,
+                            op,
+                            pos: _,
+                        } => match op {
+                            EffectOps::Jump
+                            | EffectOps::Branch
+                            | EffectOps::Call
+                            | EffectOps::Return => {
+                                res.push((&f.name, new_block));
+                                new_block = Vec::<&'a Code>::new();
+                            }
+                            _ => (),
+                        },
+                        _ => (),
+                    };
+                }
+                Code::Label { label, pos: _ } => {
+                    if used_labels.contains(label) {
+                        if new_block.len() > 0 {
+                            res.push((&f.name, new_block));
+                        }
+                        new_block = Vec::<&'a Code>::from([instr]);
+                    }
+                }
+            }
+        }
+        if new_block.len() > 0 {
+            res.push((&f.name, new_block));
+        }
+        //println!("{:?}", f.instrs);
+    }
+    //println!("{:?}", used_labels);
+    res
 }
 
 // usage: target/debug/rust_stuff test.json
@@ -33,9 +163,15 @@ fn main() {
     };
     // extract functions
 
-    for f in v.functions {
-        println!("{:?}", f.instrs);
-    }
+    let p = get_used_labels(&v);
+    let blocks = form_basic_blocks(&v, &p);
+    for (i, block) in blocks.iter().enumerate() {
+        println!("block: {} in func {}", i, block.0);
 
+        for code in block.1.iter() {
+            println!("{}", code);
+        }
+    }
+    //println!("{:?}", blocks);
     //println!("{:?}", v.functions);
 }
